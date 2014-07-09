48b922081200f410d890c1d5ecae4e36e590c1ef
Samuel Groß <dev@samuel-gross.de>
2014-07-09 14:12:04 +0200

    Added support for x86_64 and hex strings starting with '0x'

--- asm.py
+++ asm.py
@@ -15,32 +15,33 @@ import os
 from subprocess import Popen, PIPE
 
 
-@commands('disas', 'disassemble')
-@nickname_commands('disas', 'disassemble')
+@commands('disas', 'disas64', 'disassemble', 'disassemble64')
 @example('.disas 66556689e590c9c3')
 def disassemble(bot, trigger):
     """Disassemble Intel X86 machine code."""
     if not trigger.group(2):
         return bot.reply('Nothing to disassemble')
     try:
-        code = unhexlify(trigger.group(2))
+        arg = trigger.group(2)
+        code = unhexlify(arg[2:] if 'x' in arg else arg)
     except Exception:
         return bot.say('Invalid hex sequence')
+
+    bits = 64 if '64' in trigger.group(1) else 32
 
     filename = '/tmp/' + ''.join( choice(string.ascii_lowercase) for i in range(10)) + '.bin'
 
     with open(filename, 'wb') as f:
         f.write(code)
 
-    result = Popen(['ndisasm', '-b', '32', '-o', '0x1000', filename], stdout=PIPE).stdout.read()
+    result = Popen(['ndisasm', '-b', str(bits), '-o', '0x1000', filename], stdout=PIPE).stdout.read()
 
     os.remove(filename)
 
     for line in result.split('\n'):
         bot.say(line)
 
-@commands('as', 'assemble')
-@nickname_commands('as', 'assemble')
+@commands('as', 'as64', 'assemble', 'assemble64')
 @example('.as push ebp; mov ebp, esp; jmp 0x14')
 def assemble(bot, trigger):
     """Assemble Intel X86 instructions."""
@@ -48,10 +49,12 @@ def assemble(bot, trigger):
     if not code:
         return bot.reply('Nothing to assemble')
 
+    bits = 64 if '64' in trigger.group(1) else 32
+
     filename = '/tmp/' + ''.join(choice(string.ascii_lowercase) for i in range(10)) + '.asm'
 
     with open(filename, 'w') as f:
-        f.write('BITS 32\n' + re.sub(r';\s*', ';\n', code))
+        f.write('BITS %i\n' % bits + re.sub(r';\s*', ';\n', code))
 
     p = Popen(['nasm', '-f', 'bin', '-o', filename[:-4], filename])
     p.wait()
