869ecd9541fbaab3599fbb0674a43bd15ba68f52
Samuel Groß <dev@samuel-gross.de>
2014-07-09 20:54:48 +0200

    asm.py can now provide some basic information about x86 instructions

--- asm.py
+++ asm.py
@@ -18,7 +18,7 @@ from subprocess import Popen, PIPE
 @commands('disas', 'disas64', 'disassemble', 'disassemble64')
 @example('.disas 66556689e590c9c3')
 def disassemble(bot, trigger):
-    """Disassemble Intel X86 machine code."""
+    """Disassemble x86 machine code."""
     if not trigger.group(2):
         return bot.reply('Nothing to disassemble')
     try:
@@ -44,7 +44,7 @@ def disassemble(bot, trigger):
 @commands('as', 'as64', 'assemble', 'assemble64')
 @example('.as push ebp; mov ebp, esp; jmp 0x14')
 def assemble(bot, trigger):
-    """Assemble Intel X86 instructions."""
+    """Assemble x86 instructions."""
     code = trigger.group(2)
     if not code:
         return bot.reply('Nothing to assemble')
@@ -71,3 +71,69 @@ def assemble(bot, trigger):
             if hex:
                 bot.say(hex)
         os.remove(filename[:-4])
+
+def x86jmp(bot, instr):
+    """Display information about a x86 conditional jump."""
+    if instr not in jxx:
+        return bot.say('I can\'t find anything about that instruction, sorry')
+
+    bot.say('%s : %s' % (instr, jxx[instr]))
+
+def x86instr(bot, instr):
+    """Display information about any x86 instruction thats no a conditional jump."""
+    raw = web.get('http://www.felixcloutier.com/x86/')
+
+    match = re.search('<tr><td><a href="./(?P<page>[A-Z:]*).html">%s</a></td><td>(?P<desc>[^<]*)</td></tr>' % instr, raw)
+
+    if not match:
+        return bot.say('I can\'t find anything about that instruction, sorry')
+
+    bot.say('%s : %s -- %s' % (instr, match.group('desc'), 'http://www.felixcloutier.com/x86/%s' % match.group('page')))
+
+@commands('x86', 'instr', 'instruction')
+def instruction(bot, trigger):
+    """Display information about an x86 instruction."""
+    instr = trigger.group(2)
+    if not instr:
+        return bot.reply('Give me an instruction')
+
+    instr = instr.strip().upper()
+    if 'J' == instr[0] and not instr == 'JMP':
+        return x86jmp(bot, instr)
+
+    x86instr(bot, instr)
+
+jxx = {
+    'JA'   : 'Jump if above (CF=0 and ZF=0)',
+    'JAE'  : 'Jump if above or equal (CF=0)',
+    'JB'   : 'Jump if below (CF=1)',
+    'JBE'  : 'Jump if below or equal (CF=1 or ZF=1)',
+    'JC'   : 'Jump if carry (CF=1)',
+    'JCXZ' : 'Jump if CX register is 0',
+    'JECXZ': 'Jump if ECX register is 0',
+    'JRCXZ': 'Jump if RCX register is 0',
+    'JE'   : 'Jump if equal (ZF=1)',
+    'JG'   : 'Jump if greater (ZF=0 and SF=OF)',
+    'JGE'  : 'Jump if greater or equal (SF=OF)',
+    'JL'   : 'Jump if less (SF!=OF)',
+    'JLE'  : 'Jump if less or equal (ZF=1 or SF!=OF)',
+    'JNA'  : 'Jump if not above (CF=1 or ZF=1)',
+    'JNAE' : 'Jump if not above or equal (CF=1)',
+    'JNB'  : 'Jump if not below (CF=0)',
+    'JNBE' : 'Jump if not below or equal (CF=0 and ZF=0)',
+    'JNC'  : 'Jump if not carry (CF=0)',
+    'JNE'  : 'Jump if not equal (ZF=0)',
+    'JNG'  : 'Jump if not greater (ZF=1 or SF!=OF)',
+    'JNGE' : 'Jump if not greater or equal (SF!=OF)',
+    'JNL'  : 'Jump if not less (SF=OF)',
+    'JNLE' : 'Jump if not less or equal (ZF=0 and SF=OF)',
+    'JNO'  : 'Jump if not overflow (OF=0)',
+    'JNP'  : 'Jump if not parity (PF=0)',
+    'JNS'  : 'Jump if not sign (SF=0)',
+    'JNZ'  : 'Jump if not zero (ZF=0)',
+    'JO'   : 'Jump if overflow (OF=1)',
+    'JP'   : 'Jump if parity (PF=1)',
+    'JPE'  : 'Jump if parity even (PF=1)',
+    'JPO'  : 'Jump if parity odd (PF=0)',
+    'JS'   : 'Jump if sign (SF=1)'
+}
